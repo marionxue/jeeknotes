ad135958ad39759471e2224c5bc9a16f5e25aa99
marionxue <145032707@qq.com>
2019-11-25 22:41:00 +0800

    update

---
+++ python/src/极客时间-零基础学习python/08-列表的定义与使用.py
@@ -0,0 +1,6 @@
+chinese_zodiac ='猴鸡狗猪鼠牛虎兔龙蛇马羊'
+
+year =  int(input("请输入您要判断的年数: "))
+
+print(chinese_zodiac[year % 12])
+
---
+++ python/src/极客时间-零基础学习python/11-列表与元组的区别.py
@@ -0,0 +1,9 @@
+a_list=['abc','xyz']
+
+# 增加元素
+a_list.append('X')
+print(a_list)
+
+a_list.remove('xyz')
+print(a_list)
+
